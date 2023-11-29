cdb13ae5ef23b2e4194f90eba2e25b4e24232749
ishika2203 <ishikabatra.net@gmail.com>
2023-11-30 03:16:51 +0530

    click events added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log('Hello guys!!');
+  };
+  const handleClickAgain = (name) => {
+    console.log('hello ' + name);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => handleClickAgain('mario')}>Click me again</button>
     </div>
   );
 };

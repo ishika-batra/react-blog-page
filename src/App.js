332399a7bb4d806e7f9e1691c6b22a2579115594
ishika2203 <ishikabatra.net@gmail.com>
2023-11-30 00:13:28 +0530

    multiple component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = 'https://www.google.com';
-  // const person = { name: 'Ishika', age: 22 };
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
-        <p>hello</p>
-        <p>60</p>
-        <p>{[1, 2, 3, 4]}</p>
-        <p>{Math.random() * 10}</p>
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );

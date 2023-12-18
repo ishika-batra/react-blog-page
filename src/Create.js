a3d4d1b9324742d50e5f08a4c0d9c636dfa88b55
ishika2203 <ishikabatra.net@gmail.com>
2023-12-19 02:31:19 +0530

    making a post request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('durgesh');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    });
   };
   return (
     <div className="create">
@@ -33,7 +44,8 @@ const Create = () => {
           <option value="ishika">ishika</option>
           <option value="durgesh">durgesh</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog..</button>}
       </form>
     </div>
   );
